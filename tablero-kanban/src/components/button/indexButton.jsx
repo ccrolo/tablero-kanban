e01d433cecd7eb32c390ac5028cd40bdeeb3eb2f
ccrolo <cris.rom.lopez@gmail.com>
2022-02-26 22:26:23 +0100

    cambios en el botón Add

--- tablero-kanban/src/components/button/indexButton.jsx
+++ tablero-kanban/src/components/button/indexButton.jsx
@@ -1,23 +1,30 @@
 import './styleButton.css'
 import React from 'react'
 import { useState } from 'react'
-import Card from '../card/indexCard'
+
+
+// Hay que hacer que cuando no haya texto en el text area el boton verde esté deshabilitado y además con opacidad 0.7.
+
 
 function Button() {
-    let [isChecked, isCheckedUpdate] = useState(false)
 
+    let [isChecked, isCheckedUpdate] = useState(false)
+    let [isDisabled, isDisabledUpdate] = useState(true)
 
     const handle = () => {
-
         isCheckedUpdate(true)
+        isDisabledUpdate(true)
+    }
 
-
+    const handleKeyPress = () => {
+        isDisabledUpdate(false)
     }
 
     const handleSubmit = e => {
         e.preventDefault()
         localStorage.setItem('text', e.target.textarea.value);
         isCheckedUpdate(false)
+
     }
 
 
@@ -26,9 +33,11 @@ function Button() {
             <button className="button" onClick={handle}>➕</button>
             {isChecked
                 ? <form onSubmit={handleSubmit}>
-                    <textarea placeholder='Enter a note' className='text_area' type='textarea' name='textarea' ></textarea>
+                    <textarea onKeyUp={handleKeyPress} placeholder='Enter a note' className='text_area' type='textarea' name='textarea' ></textarea>
                     <section>
-                        <button type='submit' className='button_text_area add_button'>Add</button>
+                        {isDisabled
+                            ? <button disabled={true} type='submit' className='button_text_area add_button add_button-disabled'>Add</button>
+                            : <button type='submit' className='button_text_area add_button'>Add</button>}
                         <button type='submit' className='button_text_area cancel_button'>Cancel</button>
                     </section>
                 </form>
