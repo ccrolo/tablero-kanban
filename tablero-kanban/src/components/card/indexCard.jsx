79f19b926f666221a71bfba4febda5aabc5683e0
DESKTOP-1U4HJ0I\jorge <jkokeito@gmail.com>
2022-02-28 12:40:32 +0100

    ajuste de Alex para el splice

--- tablero-kanban/src/components/card/indexCard.jsx
+++ tablero-kanban/src/components/card/indexCard.jsx
@@ -7,19 +7,16 @@ import CardsContext from '../context/indexContext'
 
 function Card(props) {
     const { cardsState, updateCardsState } = useContext(CardsContext);
-    let [arrayId, arrayIdUpdate] = useState(cardsState.map( e => e.id))
+    let [arrayId, arrayIdUpdate] = useState(cardsState.map(e => e.id)) //sabiamos que esto era una gitanada
+    //Hacer find index para encontrar la id y nos da la posicion ==> esta es la buena
       
-   
-
     const handleDelete = () => {
        
         const position = arrayId.indexOf(props.id)
-        updateCardsState(cardsState.splice(position, 1))
+        cardsState.splice(position, 1)
+        updateCardsState([...cardsState])
 
         console.log(cardsState)
-
-       
-        
 
        /*  updateCardsState(cardsState.splice(position,1))
         arrayIdUpdate(arrayId.splice(position,1)) */
