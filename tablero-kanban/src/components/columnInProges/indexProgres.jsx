3a128b29bca5f8d711ac22582b547af4ca0e201c
ccrolo <cris.rom.lopez@gmail.com>
2022-03-01 20:50:39 +0100

    cambios context

--- tablero-kanban/src/components/columnInProges/indexProgres.jsx
+++ tablero-kanban/src/components/columnInProges/indexProgres.jsx
@@ -1,16 +1,31 @@
 import './styleProgres.css'
 import Card from "../card/indexCard"
 import Button from "../button/indexButton"
+import { useContext } from 'react'
+import {CardsProgress} from '../context/indexContext'
 
-function ColumnProgres(){
+function ColumnProgress() {
+    const { cardsProgress, updateCardsProgress } = useContext(CardsProgress)
+    /*  console.log(cardsState) */
+    /* localStorage.setItem('arrayCards', cardsState); */
+    const checked = false;
+
+   
+
     return (
         <div className="column_container">
-            <Button></Button>
-            <Card></Card>
-            <Card></Card>
+            
+                <div className='column_name' >
+                    <div className='div_counter'>{cardsProgress.length}</div>
+                    <h3>In Progress</h3>
+                </div>
+                <Button checked={checked}></Button>
            
+            {cardsProgress.length === 0 ? '' : cardsProgress.map((e, i) => <Card key={i} checked={e.checked} id={e.id} date={e.date} hour={e.hour} task={e.text}></Card>)}
+
+
         </div>
     )
- }
+}
 
- export default ColumnProgres;
+export default ColumnProgress;
--- tablero-kanban/src/components/columnToDo/indexColumn.jsx
+++ tablero-kanban/src/components/columnToDo/indexColumn.jsx
@@ -1,7 +1,6 @@
 import './styleColumn.css'
 import Card from "../card/indexCard"
 import Button from "../button/indexButton"
-import { useState } from 'react'
 import { useContext } from 'react'
 import CardsContext from '../context/indexContext'
 
@@ -9,6 +8,7 @@ function Column() {
     const { cardsState, updateCardsState } = useContext(CardsContext)
     /*  console.log(cardsState) */
     localStorage.setItem('arrayCards', cardsState);
+    const checked = false;
 
    
 
@@ -19,9 +19,9 @@ function Column() {
                     <div className='div_counter'>{cardsState.length}</div>
                     <h3>To Do</h3>
                 </div>
-                <Button></Button>
+                <Button checked={checked}></Button>
            
-            {cardsState.length === 0 ? '' : cardsState.map((e, i) => <Card key={i} id={e.id} date={e.date} hour={e.hour} task={e.text}></Card>)}
+            {cardsState.length === 0 ? '' : cardsState.map((e, i) => <Card key={i} checked={e.checked} id={e.id} date={e.date} hour={e.hour} task={e.text}></Card>)}
 
 
         </div>
